aa51f149ff610e575e8bd67a4de89072b3424463
github_cloudlabsuser_2050 <github_cloudlabsuser_2050@cloudlabsaiuser.com>
2024-10-16 17:55:06 +0000

    Refactorizar

---
+++ test/refactorizar.js
@@ -0,0 +1,13 @@
+function refactorizar(){
+    let languague = {
+        "en": {
+            page: "page1",
+            type: "type"
+        }
+        ,
+        "es": {
+            page: "página1",
+            type: "tipo"
+        }
+    }
+}
